8cbc7d35bec8b5c508ca44764b86e8c1f14c3122
Shuaihao Chen <chenshuaihao8619@gmail.com>
2023-07-09 10:05:51 +0800

    CSH second second commit

--- app/src/main/java/com/wuliner/unlock_team/MainActivity.kt
+++ app/src/main/java/com/wuliner/unlock_team/MainActivity.kt
@@ -3,11 +3,21 @@ package com.wuliner.unlock_team
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.slipunlock.Presenter
+import com.wuliner.unlock_team.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(),ISlipUnlock {
     private val presenter = Presenter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        presenter.saveDotArray(binding)
+
+        //给容器添加触摸事件
+        binding.container.setOnTouchListener { v, event ->
+            presenter.touchEvent(event, binding)
+            true
+        }
     }
 }
